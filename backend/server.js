506631133bcaf89a91a6ef31ec4b24d185fa8d6d
deepakjangir12345 <deepjangir9799@gmail.com>
2026-08-06 02:27:26 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,7 @@ app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/profile", profileRoutes);
 app.use("/api/password", passwordRoutes);
-app.use("/api/lessons", lessonroutes);
+app.use("/api/lessons", lessonRoutes);
 const net = require("net");
 
 app.get("/api/test-smtp", (req, res) => {
